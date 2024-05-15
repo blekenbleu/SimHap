c40d0f72f390f82be550f2131412fb106e2f1a10
Steven Carmichael <blekenbleu@gmail.com>
2024-05-15 07:04:20 -0400

    remove some JetBrains comments

--- Properties/Resources.cs
+++ Properties/Resources.cs
@@ -1,8 +1,6 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: sierses.SimHap.Properties.Resources
-// Assembly: SimHaptics, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: E01F66FE-3F59-44B4-8EBC-5ABAA8CD8267
-// Assembly location: C:\Users\demas\Downloads\dnSpy-net-win64\SimHaptics.dll
 
 using System.CodeDom.Compiler;
 using System.ComponentModel;
--- Settings.cs
+++ Settings.cs
@@ -1,8 +1,6 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: SimHaptics.Settings
-// Assembly: SimHaptics, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: E01F66FE-3F59-44B4-8EBC-5ABAA8CD8267
-// Assembly location: C:\Users\demas\Downloads\dnSpy-net-win64\SimHaptics.dll
 
 using System.Collections.Generic;
 using System.ComponentModel;
