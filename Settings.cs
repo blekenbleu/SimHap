872ca791421dec2a39223642731e0d5627d1ab15
Steven Carmichael <blekenbleu@gmail.com>
2024-05-16 15:15:32 -0400

    change blanks to tabs;  introduce SetProp()

--- Settings.cs
+++ Settings.cs
@@ -7,71 +7,63 @@ using System.ComponentModel;
 
 namespace sierses.SimHap
 {
-  public class Settings : INotifyPropertyChanged
-  {
-    public Spec Vehicle;
-    public Dictionary<string, double> EngineMult;
-    public Dictionary<string, double> RumbleMult;
-    public Dictionary<string, double> SuspensionMult;
-    public Dictionary<string, double> SuspensionGamma;
-    public Dictionary<string, double> SlipXMult;
-    public Dictionary<string, double> SlipYMult;
-    public Dictionary<string, double> SlipXGamma;
-    public Dictionary<string, double> SlipYGamma;
-    public Dictionary<string, double> Motion;
-    private bool unlocked = true;
-    private int absPulseLength;
-    private int downshiftDurationMs;
-    private int upshiftDurationMs;
+	public class Settings : INotifyPropertyChanged
+	{
+		public Spec Vehicle;
+		public Dictionary<string, double> EngineMult;
+		public Dictionary<string, double> RumbleMult;
+		public Dictionary<string, double> SuspensionMult;
+		public Dictionary<string, double> SuspensionGamma;
+		public Dictionary<string, double> SlipXMult;
+		public Dictionary<string, double> SlipYMult;
+		public Dictionary<string, double> SlipXGamma;
+		public Dictionary<string, double> SlipYGamma;
+		public Dictionary<string, double> Motion;
+		private bool unlocked = true;
+		private int absPulseLength;
+		private int downshiftDurationMs;
+		private int upshiftDurationMs;
 
-    public bool Unlocked
-    {
-      get => this.unlocked;
-      set
-      {
-        this.unlocked = value;
-        this.OnPropertyChanged("unlocked");
-      }
-    }
+		public bool Unlocked
+		{
+			get => this.unlocked;
+			set { SetProp(ref this.unlocked, value, nameof(Unlocked)); }
+		}
 
-    public int ABSPulseLength
-    {
-      get => this.absPulseLength;
-      set
-      {
-        this.absPulseLength = value;
-        this.OnPropertyChanged("absPulseLength");
-      }
-    }
+		public int ABSPulseLength
+		{
+			get => this.absPulseLength;
+			set { SetProp(ref this.absPulseLength, value, nameof(ABSPulseLength)); }
+		}
 
-    public int DownshiftDurationMs
-    {
-      get => this.downshiftDurationMs;
-      set
-      {
-        this.downshiftDurationMs = value;
-        this.OnPropertyChanged("downshiftDurationMs");
-      }
-    }
+		public int DownshiftDurationMs
+		{
+			get => this.downshiftDurationMs;
+			set { SetProp(ref this.downshiftDurationMs, value, nameof(DownshiftDurationMs)); }
+		}
 
-    public int UpshiftDurationMs
-    {
-      get => this.upshiftDurationMs;
-      set
-      {
-        this.upshiftDurationMs = value;
-        this.OnPropertyChanged("upshiftDurationMs");
-      }
-    }
+		public int UpshiftDurationMs
+		{
+			get => this.upshiftDurationMs;
+			set { SetProp(ref this.upshiftDurationMs, value, nameof(UpshiftDurationMs)); }
+		}
 
-    public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-    protected void OnPropertyChanged(string propertyName)
-    {
-      PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
-      if (propertyChanged == null)
-        return;
-      propertyChanged((object) this, new PropertyChangedEventArgs(propertyName));
-    }
-  }
+		protected bool SetProp<T>(ref T property, T value, string propertyName)
+		{
+			if (EqualityComparer<T>.Default.Equals(property, value))
+				return false;
+
+			property = value;
+			if (nameof(Unlocked) != propertyName)
+				SimHapticsPlugin.Changed = true;	
+			PropertyChangedEventHandler handle = this.PropertyChanged;
+			if (handle == null)
+				return false;
+
+			handle((object) this, new PropertyChangedEventArgs(propertyName));
+			return true;
+		}
+	}
 }
