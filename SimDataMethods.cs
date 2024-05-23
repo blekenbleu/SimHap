49a01502e4e9571c55d3eb7a668a3f976aa1dc7f
Steven Carmichael <blekenbleu@gmail.com>
2024-05-23 05:05:47 -0400

    move SetVehicle to SimDataMethods;  Lcars never null

--- SimDataMethods.cs
+++ SimDataMethods.cs
@@ -1,5 +1,5 @@
 using GameReaderCommon;
-using SimHub.Plugins;
+using SimHub;
 using System;
 using System.Collections.Generic;
 
@@ -23,8 +23,6 @@ namespace sierses.SimHap
 			bool temp;
 			string cid = car.id;
 
-			if (null == Lcars)
-				Lcars = new();
 			int Index = Lcars.FindIndex(x => x.id == cid);
 			if (temp = 0 > Index)
 				Lcars.Add(car);
@@ -35,6 +33,7 @@ namespace sierses.SimHap
 
 		public SimData()
 		{
+			Lcars = new();
 			GameAltText = "";
 			LoadText = "Not Loaded";
 			Gear = 0;
@@ -106,6 +105,162 @@ namespace sierses.SimHap
 			MotionSwayMult = GetSetting("MotionSwayMult", 1.0);
 			MotionSwayGamma = GetSetting("MotionSwayGamma", 1.0);
 		}
+
+		// called from DataUpdate() and recalled FetchCarData(), perhaps repeatedly
+		internal void SetVehicle(SimHap shp)
+		{
+			StatusDataBase db;
+
+			if (null != shp)	// null when called by FetchCarData()
+			{
+				SHP = shp;
+				db = SHP.Gdat.NewData;
+				string cid = db.CarId;
+				
+/*				// if (Settings.Vehicle != null && (Settings.Vehicle.Id == db.CarId || Settings.Vehicle.Id == db.CarModel))
+				if (db.CarId == SHP.Settings.Vehicle.Id)
+				{
+					Spec temp = new Spec(SHP.S);
+					SHP.S = SHP.Settings.Vehicle;
+					SHP.Settings.Vehicle = temp;
+
+					SimHap.FetchStatus = APIStatus.None;			// disable Index test and Import
+					SimHap.LoadStatus = DataStatus.SettingsFile;	// disable S.Defaults() in SetDefaultVehicle()
+					SimHap.LoadFinish = false;						// enable SetDefaultVehicle
+					Index = 0;										// disable GameId switch
+					Logging.Current.Info("SimHap.SetVehicle():  " + (LoadText = "Reloaded from Settings"));
+				}
+				else { */
+					Index = Lcars.FindIndex(x => x.id == cid);
+					if (0 <= Index)
+					{
+						SimHap.FetchStatus = APIStatus.Loaded;
+						SimHap.LoadFinish = false;					// enable SetDefaultVehicle
+					}
+//				}
+			}
+			db = SHP.Gdat.NewData;
+			if (0 > Index)
+				switch (SimHap.CurrentGame)
+				{
+					case GameId.AC:
+					case GameId.ACC:
+					case GameId.PC2:
+					case GameId.RBR:
+					case GameId.GTR2:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						break;
+					case GameId.AMS1:
+					case GameId.LMU:
+					case GameId.RF2:
+						if (SHP.S.Category != db.CarClass && SimHap.FailedCategory != db.CarClass)
+							SimHap.FetchCarData(this, db.CarId, db.CarClass, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						break;
+					case GameId.AMS2:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						SHP.S.Name = db.CarModel;
+						SHP.S.Category = db.CarClass;
+						break;
+					case GameId.D4:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						if (0 == SHP.S.IdleRPM)
+							SHP.S.IdleRPM = Convert.ToUInt16(10 * (int)SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.IdleRpm"));
+						break;
+					case GameId.DR2:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						if (0 == SHP.S.IdleRPM)
+							SHP.S.IdleRPM = Convert.ToUInt16(10 * (int)SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.IdleRpm"));
+						break;
+					case GameId.WRC23:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, Math.Floor(db.CarSettings_CurrentGearRedLineRPM), db.MaxRpm);
+						if (0 == SHP.S.IdleRPM)
+							SHP.S.IdleRPM = Convert.ToUInt16(SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.SessionUpdate.vehicle_engine_rpm_idle"));
+						break;
+					case GameId.F12022:
+					case GameId.F12023:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						if (0 == SHP.S.IdleRPM)
+							SHP.S.IdleRPM = Convert.ToUInt16(10 * (int)SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.PlayerCarStatusData.m_idleRPM"));
+						break;
+					case GameId.Forza:
+						SimHap.FetchCarData(this, db.CarId.Substring(4), null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						if (0 == SHP.S.IdleRPM)
+							SHP.S.IdleRPM = Convert.ToUInt16(SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.EngineIdleRpm"));
+						break;
+					case GameId.IRacing:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						GameAltText = SHP.PM.GameName + (string)SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.SessionData.WeekendInfo.Category");
+						if (0 == SHP.S.IdleRPM)
+						{
+							var rpm = SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.SessionData.DriverInfo.DriverCarIdleRPM");
+							if (null != rpm)
+								SHP.S.IdleRPM = Convert.ToUInt16(rpm);
+						}
+						break;
+					case GameId.RRRE:
+						if (SHP.S.Id != db.CarModel && SimHap.FailedId != db.CarModel)
+							SimHap.FetchCarData(this, db.CarModel, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						break;
+					case GameId.BeamNG:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S,
+								SHP.S.Redline = Convert.ToUInt16(0.5 + db.MaxRpm),
+								SHP.S.MaxRPM = Convert.ToUInt16((Math.Ceiling(db.MaxRpm * 0.001) - db.MaxRpm * 0.001) > 0.55
+									 ? Math.Ceiling(db.MaxRpm * 0.001) * 1000.0
+									 : Math.Ceiling((db.MaxRpm + 1000.0) * 0.001) * 1000.0)
+							);
+						if (0 == SHP.S.IdleRPM)
+							SHP.S.IdleRPM = Convert.ToUInt16(SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.idle_rpm"));
+						break;
+					case GameId.GPBikes:
+					case GameId.MXBikes:
+						if (SHP.S.Id != db.CarId)
+						{
+							SHP.S.Id = db.CarId;
+							SHP.S.MaxRPM = Convert.ToUInt16(0.5 + db.MaxRpm);
+							SHP.S.Redline = Convert.ToUInt16(SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.m_sEvent.m_iShiftRPM"));
+							SimHap.LoadFinish = false;
+							SimHap.FetchStatus = APIStatus.Fail;
+						}
+						break;
+					case GameId.GranTurismo7:
+					case GameId.GranTurismoSport:
+						SimHap.FetchCarData(this, db.CarId, null, SHP.S, db.CarSettings_CurrentGearRedLineRPM, db.MaxRpm);
+						SHP.S.Redline = Convert.ToUInt16(SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.MinAlertRPM"));
+						SHP.S.MaxRPM = Convert.ToUInt16(SHP.PM.GetPropertyValue("DataCorePlugin.GameRawData.MaxAlertRPM"));
+						break;
+					default:
+						SHP.S.Redline = Convert.ToUInt16(db.CarSettings_CurrentGearRedLineRPM);
+						SHP.S.MaxRPM = Convert.ToUInt16(db.MaxRpm);
+						SimHap.FetchStatus = APIStatus.Fail;
+						break;
+				}
+
+			if (!SimHap.LoadFinish && SimHap.FetchStatus != APIStatus.Waiting)
+			{
+				if (SimHap.FetchStatus == APIStatus.Loaded)
+				{
+					if (0 <= Index) {
+						SHP.S.Import(Lcars[Index]);
+						SimHap.LoadStatus = DataStatus.JSON;
+						Logging.Current.Info("SimHap.SetVehicle():  " + (LoadText = "JSON Load Success"));
+					} else {
+						SimHap.LoadStatus = DataStatus.SimHapticsAPI;
+						Logging.Current.Info("SimHap.SetVehicle():  " + (LoadText = "DB Load Success"));
+					}
+					SimHap.Changed = true;
+					SHP.Settings.Vehicle = new Spec(SHP.S);
+					SimHap.FetchStatus = APIStatus.Success;
+					SimHap.FailedId = "";
+				}
+				else SHP.SetDefaultVehicle(db); // sets LoadText
+				FinalizeVehicle();				// sets LoadFinish = true
+				if (0 > Index)
+					Add(SHP.S.Car);
+				Gears = db.CarSettings_MaxGears > 0 ? db.CarSettings_MaxGears : 1;
+				GearInterval = 1 / Gears;
+			}
+
+		}		// SetVehicle()
 
 		private void FinalizeVehicle()
 		{
