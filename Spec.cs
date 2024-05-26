5c79bdbe1be39063606bb0cac73bd23bbc30cffb
Steven Carmichael <blekenbleu@gmail.com>
2024-05-26 17:29:05 -0400

    defaults property; private Lcars, public Cars, Set

--- Spec.cs
+++ Spec.cs
@@ -50,6 +50,7 @@ namespace sierses.Sim
 		public ushort idlerpm;
 		public string category;
 		public string notes;
+		public string defaults;
 	}
 
 	// format for downloading from website; must be public
@@ -61,18 +62,18 @@ namespace sierses.Sim
 	public class ListDictionary : NotifyPropertyChanged
 	{
 		private Dictionary<string, List<CarSpec>> inDict;
-		private readonly Spec S;
+		private readonly Spec Sp;
 		internal ListDictionary(Spec s)
 		{
-			S = s;
-//			inDict = new();
+			Sp = s;
+			inDict = new();
 		}
 
-		internal bool Add()				// S.LD.Add
+		internal bool Add()				// ListDictionary: S.LD.Add
 		{
 			// handle Loaded and Save
-			S.Add();				// S.Lcars.Add for S.LD.Add()
-			List<CarSpec> s = S.Lcars;
+			Sp.Add();					// ListDictionary:  S.Add() Private_Car to Lcars
+			List<CarSpec> s = Sp.Cars;
 			if (0 == s.Count)
 				return false;
 
@@ -86,9 +87,15 @@ namespace sierses.Sim
 					Index = inDict[k].FindIndex(x => x.id == s[i].id);
 					if (0 <= Index)
 						inDict[k][Index] = s[i];
-					else inDict[k].Add(s[i]);
+					else {
+						inDict[k].Add(s[i]);		// ListDictionary:  add Sp.Cars[i] to current dictionary
+						Haptics.Save = true;
+					}
 				}
-			else inDict.Add(k, s);
+			else {
+				inDict.Add(k, s);					// ListDictionary:  add Lcars to dictionary
+				Haptics.Save = true;
+			}
 			return true;
 		}
  
@@ -98,52 +105,69 @@ namespace sierses.Sim
 			return (null != json || 0 < (inDict = new()).Count) && 0 < (inDict = json).Count;
 		}
 
+		public bool Extract(string game)
+		{
+			return 0 < inDict.Count && inDict.ContainsKey(game) && 0 < Sp.Set(inDict[game]);
+		}
+
 		internal ushort Count
 		{
 			get { return (ushort)inDict.Count; }
 		}
 
-		public bool Extract(string game)
-		{
-			return inDict.ContainsKey(game) && 0 < (S.Lcars = inDict[game]).Count;
-		}
-
 		internal string Jstring()	// ignore null (string) values; indent JSON
 		{
-			return JsonConvert.SerializeObject(S.LD.inDict, new JsonSerializerSettings
+			return JsonConvert.SerializeObject(Sp.LD.inDict, new JsonSerializerSettings
 			{ Formatting = Formatting.Indented, NullValueHandling = NullValueHandling.Ignore });
 		}
-	}	// class ListDictionary
+	}   // class ListDictionary
 
 	public class Spec : NotifyPropertyChanged
 	{
 		private CarSpec Private_Car { get; set; }
 		internal CarSpec Car { get => Private_Car; }
-		internal List<CarSpec> Lcars;
-		public ListDictionary LD { get; set; }	// needs to be public for JsonConvert
-		//private Haptics SHP;
-
-		public void Init()
-		{
-			Lcars = new();						// required 24 May 2024
-			LD = new(this); 
-		}
+		private List<CarSpec> Lcars;
+		internal List<CarSpec> Cars { get => Lcars; }
+		public ListDictionary LD { get; set; }  // needs to be public for JsonConvert
 
 		public Spec()
 		{
-			Private_Car = new();				// required 24 May 2024
+			Private_Car = new();                // required 24 May 2024
+			Lcars = new();                      // required 24 May 2024
+			LD = new(this);
 		}
 
-		public Spec(Spec s)
+		internal int Lcount { get => Lcars.Count; }
+
+        internal bool Set(Spec s)
 		{
+			if (null == s.Private_Car)
+			{
+				Logging.Current.Info("Haptics.Spec.Set(Spec s):  null Car");
+				return false;
+			}
 			Private_Car = s.Private_Car;
+			return true;
+		}
+
+		internal int Set(List<CarSpec> l)
+		{
+			if (null == l || 1 > l.Count)
+			{
+				Logging.Current.Info("Haptics.Spec.Set(List<CarSpec>):  empty List");
+				return -1;
+			}
+			return (Lcars = l).Count;
 		}
 
 		internal void Add()				// S.Add():  add or update Private_Car in Lcars
 		{
 			Haptics.Loaded = false;		// done with this car;  update Save
 			if ((null == Private_Car.id) || (null == Private_Car.game) || (null == Private_Car.name))
+			{
+				Logging.Current.Info("Haptics.Spec.Add(Car) : missing essential elements");
 				return;
+			}
 
 			string cid = Private_Car.id;
 
@@ -225,6 +249,11 @@ namespace sierses.Sim
 				Haptics.Save = true;
 				Lcars[Index].idlerpm = Private_Car.idlerpm;
 			}
+			if (Lcars[Index].defaults != Private_Car.defaults)
+			{
+				Haptics.Save = true;
+				Lcars[Index].defaults = Private_Car.defaults;
+			}
 			if (Lcars[Index].category != Private_Car.category)
 			{
 				Haptics.Save = true;
@@ -237,15 +266,21 @@ namespace sierses.Sim
 			}
 		}
 
-		internal bool Set(Download dl, ushort gameRedline, ushort gameMaxRPM) // called by FetchCarData()
+		internal bool Set(Download dl, ushort gameRedline, ushort gameMaxRPM) // called >>only<< by FetchCarData()
 		{
 			if (null == dl || null == dl.data || 0 == dl.data.Count)
+			{
+//				Logging.Current.Info("Spec.Set(Download):  empty");	// 
 				return false;
+			}
 
 			CarSpec data = dl.data[0];
 
 			if (null == data.name || null == data.id)
+			{
+				Logging.Current.Info("Spec.Set(Download):  empty CarSpec");
 				return false;
+			}
 
 			Game = Haptics.GameDBText;
 			Id = Haptics.CurrentGame == GameId.Forza ? "Car_" + data.id : data.id;	// Set()
@@ -273,13 +308,12 @@ namespace sierses.Sim
 
 		internal string Defaults(StatusDataBase db)	
 		{
+			if (null == Haptics.GameDBText)
+				return $"Haptics.Spec.Defaults({db.CarId}):  null GameDBText";
+
 			string StatusText;
 
-			if (null == Haptics.GameDBText)
-				return "Haptics.Spec.Defaults():  null GameDBText";
-
-			Logging.Current.Info($"Haptics.Defaults({db.CarId}): "
-								+(Haptics.Loaded ? " Loaded " : "") + (Haptics.Waiting ? " Waiting" : ""));
+			Default = "Defaults";
 			Game = Haptics.GameDBText;
 			CarName = db.CarModel;				// Defaults()
 			Category = db.CarClass;
@@ -307,12 +341,12 @@ namespace sierses.Sim
 				case GameId.RBR:
 				case GameId.RF2:
 				case GameId.BeamNG:
-					StatusText = "Unavailable: using generic car";
+					StatusText = "unavailable: using generic car";
 					break;
 				case GameId.D4:
 				case GameId.DR2:
 				case GameId.WRC23:
-					StatusText = "Unavailable: using generic Rally2";
+					StatusText = "unavailable: using generic Rally2";
 					EngineConfiguration = "I";
 					EngineCylinders = 4;
 					EngineLocation = "F";
@@ -324,7 +358,7 @@ namespace sierses.Sim
 					break;
 				case GameId.F12022:
 				case GameId.F12023:
-					StatusText = "Unavailable: using generic F1";
+					StatusText = "unavailable: using generic F1";
 					EngineConfiguration = "V";
 					EngineCylinders = 6;
 					EngineLocation = "RM";
@@ -335,7 +369,7 @@ namespace sierses.Sim
 					MaxTorque = 650;
 					break;
 				case GameId.KK:
-					StatusText = "Unavailable: using generic Kart";
+					StatusText = "unavailable: using generic Kart";
 					EngineConfiguration = "I";
 					EngineCylinders = 1;
 					EngineLocation = "RM";
@@ -346,7 +380,7 @@ namespace sierses.Sim
 					MaxTorque = 24;
 					break;
 				case GameId.GPBikes:
-					StatusText = "Unavailable: using generic Superbike";
+					StatusText = "unavailable: using generic Superbike";
 					EngineConfiguration = "I";
 					EngineCylinders = 4;
 					EngineLocation = "M";
@@ -357,7 +391,7 @@ namespace sierses.Sim
 					MaxTorque = 100;
 					break;
 				case GameId.MXBikes:
-					StatusText = "Unavailable: using generic MX Bike";
+					StatusText = "unavailable: using generic MX Bike";
 					EngineConfiguration = "I";
 					EngineCylinders = 1;
 					EngineLocation = "M";
@@ -369,7 +403,7 @@ namespace sierses.Sim
 					break;
 				case GameId.GranTurismo7:
 				case GameId.GranTurismoSport:
-					StatusText = "Unavailable: assume 500HP 4 Liter V6";
+					StatusText = "unavailable: assume 500HP 4 Liter V6";
 					EngineConfiguration = "V";
 					EngineCylinders = 6;
 					EngineLocation = "RM";
@@ -380,7 +414,7 @@ namespace sierses.Sim
 					MaxTorque = 400;
 					break;
 				default:
-					StatusText = $"Specs unavailable for {Haptics.CurrentGame}";
+					StatusText = $"specs unavailable for {Haptics.CurrentGame}";
 					break;
 			}
 			if (0 == Redline)
@@ -392,9 +426,9 @@ namespace sierses.Sim
 			Id =									// Defaults()
 				(GameId.RRRE == Haptics.CurrentGame || GameId.D4 == Haptics.CurrentGame || GameId.DR2 == Haptics.CurrentGame) ?
 					db.CarModel : db.CarId;
-			if (0 < StatusText.Length)
-				Logging.Current.Info($"Haptics.Defaults({Haptics.CurrentGame}, {db.CarModel}): "
-								   + (Haptics.Loaded ? " Loaded" : "") + (Haptics.Waiting ? " Waiting" : "") + ":  " + StatusText);
+			Logging.Current.Info($"Haptics.Defaults({db.CarId}/{Id}): " +
+								(Haptics.Save ? " Haptics.Save " : "") + (Haptics.Loaded ? " Loaded " : "") + (Haptics.Waiting ? " Waiting" : "")
+								 + $":  {Haptics.CurrentGame}, {db.CarModel}" + " " + (0 < StatusText.Length ? StatusText : ""));
 			return StatusText;
 		}	// Defaults()
 
@@ -414,6 +448,12 @@ namespace sierses.Sim
 		{
 			get => Private_Car.id;
 			set { SetField(ref Private_Car.id, value, nameof(Id)); }
+		}
+
+		public string Default
+		{
+			get => Private_Car.defaults;
+			set { SetField(ref Private_Car.defaults, value, nameof(Default)); }
 		}
 
 		public string Category
