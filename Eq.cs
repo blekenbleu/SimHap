02d698cfbd98c8a781db926e406e009cb2d4640c
Steven Carmichael <blekenbleu@gmail.com>
2024-06-05 05:29:13 -0400

    repair Geq()

--- Eq.cs
+++ Eq.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
-
 namespace sierses.Sim
 {
-	public class Tone
+    public class Tone
 	{
 		internal ushort[] Freq;
 	}
@@ -16,25 +14,26 @@ namespace sierses.Sim
 	public class Geq
 	{
 		public ushort Equalizer(ushort rpm, ushort[][] LUT)
-	
-		if (rpm < LUT[0] || rpm > LUT[1])
-			return 0;
+		{
+			int l = LUT[1].Length - 1;
+            if (rpm < LUT[1][0] || rpm > LUT[1][l])
+				return 0;
 
-		// LUT is unequally-spaced values)
-		for (int i = 2; i < LUT[1].Length; i++
-			if (rpm >= LUT[1][i])
-				break;
-		if (i == (LUT[1].Length - 1))
-			return LUT[0][i];
+			int i;
+            // LUT is unequally-spaced values)
+            for (i = 1; i <= l; i++)
+				if (rpm >= LUT[1][i])
+					break;
+			if (i == l)
+				return LUT[0][i];
 
-		ushort range = LUT[1][0] - LUT[0][0], l = LUT[0].Length - 1;
-		ushort below = LUT[0][i];
-		ushort above = LUT[0][i + 1];
-		ushort step = LUT[1][i + 1] - LUT[1][i];
-		// linear interpolation among non=linear points.
-		// better would be precalculated circle centers,
-		// then use Bresenham’s circle drawing algorithm at run time
-		return below + (step + 2 * (rpm - LUT[1][i]) * (above - below))
-						/  (step * 2);
+			ushort below = LUT[0][i];
+			ushort above = LUT[0][i + 1];
+			int step = (LUT[1][i + 1] - LUT[1][i]);
+			// linear interpolation among non-linear points.
+			// better would be precalculated circle centers,
+			// then Bresenham’s circle drawing algorithm at run time
+			return (ushort)(below + (step + 2 * (rpm - LUT[1][i]) * (above - below)) / (step * 2));
+		}
 	}
 }
