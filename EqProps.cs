599d49ca0111a9d7fbd1edffa4cef84ef91c37e2
Steven Carmichael <blekenbleu@gmail.com>
2024-06-11 16:40:32 -0400

    rename properties

--- EqProps.cs
+++ EqProps.cs
@@ -1,17 +1,14 @@
-using MathNet.Numerics.Interpolation;
 using SimHub.Plugins;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace sierses.Sim
 {
 	public partial class Geq
 	{
-		internal void Init(Settings Settings, Haptics h)
+		internal void Init(Engine Engine, Haptics h)
 		{
 			H = h;
-  			if (null == Settings.Engine || null == Settings.Engine.Tones
-			 || null == Settings.Engine.Tones[1].Freq || 0 == Settings.Engine.Tones[1].Freq.Count)
+  			if (null == Engine || null == Engine.Tones
+			 || null == Engine.Tones[1].Freq || 0 == Engine.Tones[1].Freq.Count)
   			{
 				Tones[0] = new();
 				Tones[0].Freq.Add(1);	// engine RPM / 60
@@ -32,35 +29,36 @@ namespace sierses.Sim
 				Tones[1].Freq.Add(4);
 				Tones[1].Freq.Add(1);
   			}
-  			else Tones = Settings.Engine.Tones;
+  			else Tones = Engine.Tones;
 
-			if (null != Settings.Engine && null != Settings.Engine.Sliders)
-				Q = Settings.Engine.Sliders;
-            if (1 > Q.Count || 9 != Q[0].Slider.Count)
+			if (null != Engine && null != Engine.Sliders)
+				Q = Engine.Sliders;
+            if (1 > Q.Count || 9 != Q[0].Slider.Length)
 			{
 				ushort s = 50, highpass = 20, lowpass = 900;
-				ObservableCollection<ushort> S = new() { highpass, s, s, s, s, s, s, s, lowpass };
+				ushort[] S = { highpass, s, s, s, s, s, s, s, lowpass };
 				if (1 > Q.Count)
                 	Q.Add(new Eq() { Slider = S });
 				else Q[0] = new Eq() { Slider = S };
 			}
 //			H.SC.Init(Q[0].Slider);
-			H.AttachDelegate("Fr0", () => Fr(0));
-			H.AttachDelegate("Fr1", () => Fr(1));
-			H.AttachDelegate("Fr2", () => Fr(2));
-			H.AttachDelegate("Fr3", () => Fr(3));
-			H.AttachDelegate("Fr4", () => Fr(4));
-			H.AttachDelegate("Fr5", () => Fr(5));
-			H.AttachDelegate("Fr6", () => Fr(6));
-			H.AttachDelegate("Fr7", () => Fr(7));
-			H.AttachDelegate("Fa0", () => Tones[1].Freq[0]);
-			H.AttachDelegate("Fa1", () => Tones[1].Freq[1]);
-			H.AttachDelegate("Fa2", () => Tones[1].Freq[2]);
-			H.AttachDelegate("Fa3", () => Tones[1].Freq[3]);
-			H.AttachDelegate("Fa4", () => Tones[1].Freq[4]);
-			H.AttachDelegate("Fa5", () => Tones[1].Freq[5]);
-			H.AttachDelegate("Fa6", () => Tones[1].Freq[6]);
-			H.AttachDelegate("Fa7", () => Tones[1].Freq[7]);
+			H.AttachDelegate("E.Fr0", () => Fr(0));
+			H.AttachDelegate("E.Fr1", () => Fr(1));
+			H.AttachDelegate("E.Fr2", () => Fr(2));
+			H.AttachDelegate("E.Fr3", () => Fr(3));
+			H.AttachDelegate("E.Fr4", () => Fr(4));
+			H.AttachDelegate("E.Fr5", () => Fr(5));
+			H.AttachDelegate("E.Fr6", () => Fr(6));
+			H.AttachDelegate("E.Fr7", () => Fr(7));
+			H.AttachDelegate("E.Fa0", () => Tones[1].Freq[0]);
+			H.AttachDelegate("E.Fa1", () => Tones[1].Freq[1]);
+			H.AttachDelegate("E.Fa2", () => Tones[1].Freq[2]);
+			H.AttachDelegate("E.Fa3", () => Tones[1].Freq[3]);
+			H.AttachDelegate("E.Fa4", () => Tones[1].Freq[4]);
+			H.AttachDelegate("E.Fa5", () => Tones[1].Freq[5]);
+			H.AttachDelegate("E.Fa6", () => Tones[1].Freq[6]);
+			H.AttachDelegate("E.Fa7", () => Tones[1].Freq[7]);
+			AddProps(H, EqSpline(Q[0].Slider));
 		}
 
 		public string Broadcast(Haptics This, int L)
@@ -69,37 +67,37 @@ namespace sierses.Sim
 			switch (L)
 			{
 				case 1:
-					This.AttachDelegate("Eq0.0", () => Play(This, 0));
-					This.AttachDelegate("Eq0.1", () => Play(This, 1));
-					This.AttachDelegate("Eq0.2", () => Play(This, 2));
-					This.AttachDelegate("Eq0.3", () => Play(This, 3));
-					This.AttachDelegate("Eq0.4", () => Play(This, 4));
-					This.AttachDelegate("Eq0.5", () => Play(This, 5));
-					This.AttachDelegate("Eq0.6", () => Play(This, 6));
-					This.AttachDelegate("Eq0.7", () => Play(This, 7));
-					s = "Eq0 added";
+					This.AttachDelegate("E.Q0.0", () => Play(This, 0));
+					This.AttachDelegate("E.Q0.1", () => Play(This, 1));
+					This.AttachDelegate("E.Q0.2", () => Play(This, 2));
+					This.AttachDelegate("E.Q0.3", () => Play(This, 3));
+					This.AttachDelegate("E.Q0.4", () => Play(This, 4));
+					This.AttachDelegate("E.Q0.5", () => Play(This, 5));
+					This.AttachDelegate("E.Q0.6", () => Play(This, 6));
+					This.AttachDelegate("E.Q0.7", () => Play(This, 7));
+					s = "E.Q0 added";
 					break;
 				case 2:
-					This.AttachDelegate("Eq1.0", () => Play(This, 8));
-					This.AttachDelegate("Eq1.1", () => Play(This, 9));
-					This.AttachDelegate("Eq1.2", () => Play(This, 10));
-					This.AttachDelegate("Eq1.3", () => Play(This, 11));
-					This.AttachDelegate("Eq1.4", () => Play(This, 12));
-					This.AttachDelegate("Eq1.5", () => Play(This, 13));
-					This.AttachDelegate("Eq1.6", () => Play(This, 14));
-					This.AttachDelegate("Eq1.7", () => Play(This, 15));
-					s = "Eq1 added";
+					This.AttachDelegate("E.Q1.0", () => Play(This, 8));
+					This.AttachDelegate("E.Q1.1", () => Play(This, 9));
+					This.AttachDelegate("E.Q1.2", () => Play(This, 10));
+					This.AttachDelegate("E.Q1.3", () => Play(This, 11));
+					This.AttachDelegate("E.Q1.4", () => Play(This, 12));
+					This.AttachDelegate("E.Q1.5", () => Play(This, 13));
+					This.AttachDelegate("E.Q1.6", () => Play(This, 14));
+					This.AttachDelegate("E.Q1.7", () => Play(This, 15));
+					s = "E.Q1 added";
 					break;
 				case 3:
-					This.AttachDelegate("Eq2.0", () => Play(This, 16));
-					This.AttachDelegate("Eq2.1", () => Play(This, 17));
-					This.AttachDelegate("Eq2.2", () => Play(This, 18));
-					This.AttachDelegate("Eq2.3", () => Play(This, 19));
-					This.AttachDelegate("Eq2.4", () => Play(This, 20));
-					This.AttachDelegate("Eq2.5", () => Play(This, 21));
-					This.AttachDelegate("Eq2.6", () => Play(This, 22));
-					This.AttachDelegate("Eq2.7", () => Play(This, 23));
-					s = "Eq2 added; max equalizer supported...";
+					This.AttachDelegate("E.Q2.0", () => Play(This, 16));
+					This.AttachDelegate("E.Q2.1", () => Play(This, 17));
+					This.AttachDelegate("E.Q2.2", () => Play(This, 18));
+					This.AttachDelegate("E.Q2.3", () => Play(This, 19));
+					This.AttachDelegate("E.Q2.4", () => Play(This, 20));
+					This.AttachDelegate("E.Q2.5", () => Play(This, 21));
+					This.AttachDelegate("E.Q2.6", () => Play(This, 22));
+					This.AttachDelegate("E.Q2.7", () => Play(This, 23));
+					s = "E.Q2 added; max equalizer supported...";
 					break;
 				default:
 					s = "equalizer limit exceeded";
